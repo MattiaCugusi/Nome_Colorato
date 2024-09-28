c6db59ffe82cb2215b72f9a78977c9b2585f4ec8
Mattia Cugusi <m.cugusi287gmail.com>
2024-09-28 10:43:11 +0200

    generazione di r,g,b

---
+++ NomeColorato.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nome colorato</title>
+</head>
+<body>
+    <?php
+
+    $nome = "Mattia";
+    $r = rand(0,255);
+    $g = rand(0,255);
+    $b = rand(0,255);
+    
+
+
+    ?>
+    
+</body>
+</html>
